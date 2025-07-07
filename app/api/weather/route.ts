527fe954c0896cb847257a6d9a112b77f2ab9406
Atd27 <y518a480@gmail.com>
2025-07-07 23:34:32 +0900

    天気情報取得API&天気情報作成API

--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -1,12 +1,52 @@
-import prisma from "@prisma/client";
-import { Router } from "next/router";
+import { PrismaClient } from "@prisma/client";
+import { data } from "framer-motion/client";
+import { NextResponse } from "next/server";
 
-const router = Router;
+const prisma = new PrismaClient();
 
-export async function get() {
+// 天気情報取得（openweathermap api）
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const city = searchParams.get("city") || "Tokyo";
+
   const apiKey = process.env.OPENWEATHER_API_KEY;
 
-  const weatherAPI = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${apiKey}`
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?${encodeURIComponent(
+      city
+    )}&appid=${apiKey}&units=metric&lang=ja`
+  );
+
+  const data = await response.json();
+
+  return NextResponse.json(
+    {
+      success: true,
+      message: `${city}の天気情報`,
+      data: data,
+    },
+    { status: 200 }
+  );
+}
+
+// 天気情報作成
+export async function POST(request: Request) {
+  const { city, weather, temperature, observed_at } = await request.json();
+
+  const weathers = await prisma.weather.create({
+    data: {
+      city,
+      weather,
+      temperature,
+      observed_at,
+    },
+  });
+  return NextResponse.json(
+    {
+      success: true,
+      message: `${city}の天気情報`,
+      data: weathers,
+    },
+    { status: 201 }
   );
 }
