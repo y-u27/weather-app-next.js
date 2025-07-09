0693b0c0cabc9bc05c232d438c5269b131b65a54
Atd27 <y518a480@gmail.com>
2025-07-09 19:42:29 +0900

    Weatherコンポーネントの作成

---
+++ app/components/Weather.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { Box, Button, Input } from "@chakra-ui/react";
+import { useState } from "react";
+
+const Weather = () => {
+  const [city, setCity] = useState("");
+
+  async function fetchAllWeather() {
+    const res = await fetch(
+      `http://localhost:3000/api/weather?city=${encodeURIComponent(city)}`,
+      {
+        cache: "no-store",
+      }
+    );
+    const data = await res.json();
+    console.log(data);
+    
+  }
+
+  return (
+    <Box>
+      <Box boxSize="20%" p={5} display="flex">
+        <Input onChange={(e) => setCity(e.target.value)} />
+        <Button onClick={fetchAllWeather}>検索</Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default Weather;
