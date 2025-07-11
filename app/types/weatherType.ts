721de6b2d64d44b0b8f92328c7a057a66233e207
Atd27 <y518a480@gmail.com>
2025-07-11 23:20:53 +0900

    日時の型指定追加

--- app/types/weatherType.ts
+++ app/types/weatherType.ts
@@ -1,7 +1,7 @@
 export interface WeatherType {
-  id:number
-  city: string
-  weather: string
-  temperature:number
-  observed_at: Date
+  id: number;
+  city: string;
+  weather: string;
+  temperature: number;
+  observed_at: Date | string;
 }
