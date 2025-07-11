0e727719e08f9f0d14bb4035b2161a2eaab2a2e8
Atd27 <y518a480@gmail.com>
2025-07-11 23:20:30 +0900

    追加

--- app/components/Weather.tsx
+++ app/components/Weather.tsx
@@ -1,27 +1,55 @@
 "use client";
 
-import { Box, Button, Card, CardBody, Input, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { Box, Button, Card, CardBody, Select, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { WeatherType } from "../types/weatherType";
+
+type City = {
+  id: number;
+  name: string;
+};
 
 const Weather = () => {
   const [city, setCity] = useState("");
-  const [weather, setWeather] = useState(null);
+  const [weather, setWeather] = useState<WeatherType | null>(null);
+  const [cities, setCities] = useState<City[]>([]);
 
   async function fetchAllWeather() {
-    const res = await fetch(
+    const resWeather = await fetch(
       `http://localhost:3000/api/weather?city=${encodeURIComponent(city)}`,
       {
         cache: "no-store",
       }
     );
-    const data = await res.json();
-    console.log(data);
+    const dataWeather = await resWeather.json();
+    setWeather(dataWeather.data);
   }
+
+  useEffect(() => {
+    const fetchSelectCity = async () => {
+      const resCity = await fetch(`http://localhost:3000/api/city`, {
+        cache: "no-store",
+      });
+      const dataCity = await resCity.json();
+      setCities(dataCity);
+      console.log(dataCity);
+    };
+    fetchSelectCity();
+  }, []);
 
   return (
     <Box>
       <Box boxSize="20%" p={5} display="flex">
-        <Input onChange={(e) => setCity(e.target.value)} />
+        <Select
+          placeholder="都市を選択"
+          onChange={(e) => setCity(e.target.value)}
+        >
+          {cities.map((city: { id: number; name: string }) => (
+            <option key={city.id} value={city.name}>
+              {city.name}
+            </option>
+          ))}
+        </Select>
         <Button onClick={fetchAllWeather}>検索</Button>
       </Box>
 
@@ -29,10 +57,10 @@ const Weather = () => {
       {weather && (
         <Card>
           <CardBody>
-            <Text>都市名：</Text>
-            <Text>天気：</Text>
-            <Text>気温</Text>
-            <Text>日時：</Text>
+            <Text>都市名：{city}</Text>
+            <Text>天気：{weather.weather}</Text>
+            <Text>気温：{weather.temperature}</Text>
+            <Text>日時：{new Date(weather.observed_at).toLocaleString()}</Text>
           </CardBody>
         </Card>
       )}
