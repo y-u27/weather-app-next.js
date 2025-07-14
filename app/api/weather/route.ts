5fa73f2b06125000222a74d1d20db87a2421923b
Atd27 <y518a480@gmail.com>
2025-07-14 23:15:14 +0900

    同一日・地域の条件分け実装

--- app/api/weather/route.ts
+++ app/api/weather/route.ts
@@ -6,6 +6,39 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const city = searchParams.get("city") || "Tokyo";
 
+  const today = new Date();
+  // 同日判定のために時刻を00:00にリセット
+  today.setHours(0, 0, 0, 0);
+
+  const cityRecord = await prisma.city.findUnique({
+    where: { name: city },
+  });
+
+  if (!cityRecord) {
+    throw new Error("都市が存在しません");
+  }
+
+  // 今日の天気情報がDBにあるかを確認
+  const existingWeather = await prisma.weather.findFirst({
+    where: {
+      cityId: cityRecord.id,
+      observed_at: { gte: today },
+    },
+  });
+
+  // もしすでにあるなら、天気APIを呼ばずに以下を返す
+  if (existingWeather) {
+    return NextResponse.json(
+      {
+        success: true,
+        message: `${city}の天気情報（DBから取得）`,
+        data: existingWeather,
+      },
+      { status: 200 }
+    );
+  }
+
+  // DBに事前の天気情報がない場合→天気APIを呼ばない場合
   const apiKey = process.env.OPENWEATHER_API_KEY;
 
   const response = await fetch(
@@ -33,14 +66,6 @@ export async function GET(request: Request) {
   const weather = savedWeather.weather[0].main;
   const temperature = Math.round(savedWeather.main.temp);
   const observed_at = new Date();
-
-  const cityRecord = await prisma.city.findUnique({
-    where: { name: city },
-  });
-
-  if (!cityRecord) {
-    throw new Error("都市が存在しません");
-  }
 
   // データベース保存
   const createWeather = await prisma.weather.create({
