8027c78e3a1a530ed6e1e65bfdcbfe2ef3bedd27
Atd27 <y518a480@gmail.com>
2025-07-17 21:33:07 +0900

    Weatherテーブルのcityカラム削除

---
+++ prisma/migrations/20250717123225_remove_city_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `city` on the `Weather` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Weather" DROP COLUMN "city";
